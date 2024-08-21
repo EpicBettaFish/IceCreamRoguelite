99a6187451ee361b765055e0c328af7660fb0173
LeftyAtHeart <62956483+LeftyAtHeart@users.noreply.github.com>
2024-08-21 19:29:14 -0400

    shop stuff

--- ice-cream-roguelite/Scripts/purchaseable.gd
+++ ice-cream-roguelite/Scripts/purchaseable.gd
@@ -3,3 +3,4 @@ extends TextureRect
 @export var price: int
 @export_enum("spray", "swatter", "coolant", "fan") var item: int
 @export var empty_sprite: Texture
+@export var info_resource: Resource
--- ice-cream-roguelite/Scripts/shop.gd
+++ ice-cream-roguelite/Scripts/shop.gd
@@ -5,17 +5,26 @@ var hovered_icon = null
 @onready var hover_sound = $hover
 @onready var press_sound = $press
 
+@onready var info_title = $InfoPanel/title
+@onready var info_price = $InfoPanel/price
+@onready var info_description = $InfoPanel/description
+
+var clicked_icon = null
+
 func _ready():
 	for i in $items.get_child_count():
-		$items.get_child(i-1).connect("gui_input", item_click.bind($items.get_child(i-1).item))
+		$items.get_child(i-1).connect("gui_input", item_click.bind($items.get_child(i-1).item, $items.get_child(i-1)))
 
 
-func item_click(event, item):
+func item_click(event, item, node):
 	if hovered_icon != item:
 		hovered_icon = item
 		hover_sound.play()
-	if event.is_action_pressed("click"):
+	if event.is_action_pressed("click") and clicked_icon != item:
 		press_sound.play()
+		info_description.text = node.info_resource.description
+		info_title.text = node.info_resource.title
+		info_price.text = str("$ ", node.info_resource.price)
 
 func add_cones(button):
 	Singleton.money -= 1
